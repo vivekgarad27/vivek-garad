0dcb4973373f58595c8732ac6ce04ec556be70cf
Vivek Garad <vivekgarad27@gmail.com>
2025-08-06 23:09:07 +0530

    Delete test 1 directory

--- test 1/kmIntoMeters.py
+++
@@ -1,6 +0,0 @@
-##WAP to convert km into meters and centimeters
-km = int(input("Please enter the distance in km: "))
-
-meters = km * 1000
-centimeters = meters * 100
-print(f"the distance  {meters} meters and {centimeters} centimeters")
